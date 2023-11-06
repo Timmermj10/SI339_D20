4266c3731ec324dd70f4e5cf7718314464f60968
Jacob Timmerman <timmermj@umich.edu>
2023-11-06 00:08:00 -0500

    Linked JS

--- js/video.js
+++ js/video.js
@@ -1,11 +1,11 @@
 var video;
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
+// document.querySelector("#play").addEventListener("click", function () {
 // 	console.log("Play Video");
 // });
 
